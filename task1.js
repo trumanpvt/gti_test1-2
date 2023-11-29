5c04e1dd2811afc895a0ec056ea5e9acd8d95452
trumanpvt <trumanpvt@gmail.com>
2023-11-29 12:39:44 +0300

    Task 1

---
+++ task1.js
@@ -0,0 +1,24 @@
+const getRandomArray = (length) => {
+  // да, это генератор массива любых, а не рациональных чисел, иначе написание задачи займет намного больше времени
+  return Array.from({ length }, () => Math.floor(Math.random() * 100));
+};
+
+const getPercentsArr = (arr) => {
+  const maxLength = 30000000;
+
+  if (arr.length > maxLength) {
+    console.log("array is too big, result will be empty");
+    return [];
+  }
+
+  const sum = arr.reduce((prev, curr) => prev + parseFloat(curr), 0);
+
+  return arr.map((item) => (100 / (sum / item)).toFixed(3));
+};
+
+const randomArray = getRandomArray(100);
+// const start = new Date().getTime(); // при желании измерить примерное время выполнения
+const percentsArr = getPercentsArr(randomArray);
+// const end = new Date().getTime(); // при желании измерить примерное время выполнения
+// console.log(`SecondWay: ${end - start}ms`); // при желании измерить примерное время выполнения
+console.log("result: ", percentsArr);
